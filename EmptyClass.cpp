6cbf45b33e0e56665f29d9617d05c28fee8ac9b0
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-06-01 15:19:28 +0530

    Add files via upload

---
+++ EmptyClass.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+class Demo
+{
+
+    public:
+    void Display()
+    {
+        cout<<"Inside Display\n";
+
+    }
+     
+}
+
+
+  int main()
+
+{
+Demo dobj;
+
+cout<<sizeof(dobj);
+
+
+return 0;
+
+
+}
---
+++ Ganesh.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"jay ganesh";
+
+    return 0;
+}
