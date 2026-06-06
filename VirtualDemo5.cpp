a8c3efaabe2df693304e3417d75e65c19afc1194
Prajakta Jagtap <prajaktajagtap274@gmail.com>
2026-06-06 13:43:40 +0530

    Add files via upload

---
+++ VirtualDemo5.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+
+using namespace std;
+
+#pragma pack(1)
+class Base
+{
+
+    public:
+
+
+    int i,j;
+    void fun()
+    {
+        cout<<"Inside Base fun\n";
+    }
+
+    void gun()
+    {
+        cout<<"Inside Base gun\n";
+    }
+
+
+
+     virtual void sun()
+    {
+        cout<<"Inside Base sun\n";
+    }
+
+
+
+    void bun()
+    {
+        cout<<"Inside Base bun\n";
+    }
+
+};
+
+
+class Derived : public Base
+{
+      public:
+        int x;
+        void gun()
+    {
+        cout<<"Inside Derived gun\n";
+    }
+
+    void sun()
+    {
+        cout<<"Inside Derived sun\n";
+    }
+    void run()
+    {
+        cout<<"Inside Derived run\n";
+    }
+    void mun()
+    {
+        cout<<"Inside Derived mun\n";
+    }
+
+
+
+};
+
+
+  int main()
+
+{
+
+    cout<<sizeof(Base)<<"\n";
+    cout<<sizeof(Derived)<<"\n";
+
+   Base *bp = new Derived();   //upcasting
+
+
+   bp->fun();  // Base fun
+   bp->gun();  //Base gun
+   bp->sun();  //Derived sun
+   //bp->run();  //Error
+   //bp->mun();  //Error
+   bp->bun();  //base bun
+   
+   
+
+
+return 0;
+
+}
